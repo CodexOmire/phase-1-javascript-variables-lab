01b26875f3864d8a98afba234183ba6f6320d3b7
Liz Burton <liz.burton147@gmail.com>
2023-11-20 09:48:51 -0500

    Defines companyName, mostProfitableNeighborhood, and companyCeo variables to pass all tests

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+// Code your solution in this file!n
+
+const companyName = 'Scuber'; // 1. const variable
+
+let mostProfitableNeighborhood = 'Chelsea'; // 2. let variable
+
+let companyCeo = 'Susan Smith'; // 3. let variable
